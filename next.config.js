4689b6b1cdba76cfc077580bab4db947dd223cf0
Daniel Angelo <102752827+Dn099z1@users.noreply.github.com>
2024-09-17 19:27:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  webpack5: true,
-}
+  reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!isServer) {
+      config.plugins.push(new MiniCssExtractPlugin());
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
